a5df3e45b35d01e0fc8491c97ffaccf1f5095f99
= <=>
2025-02-12 16:54:51 +0100

    remove a test file used for logs

--- src/nQueenAttack/nQueenAttack.ts
+++ src/nQueenAttack/nQueenAttack.ts
@@ -1,6 +1,6 @@
 export function solveNQueenAtack(n: number): string[][] {
     if (n < 0) throw new Error("n should'nt be negative");
-    if (n === 1) return [[]];
+      if (n === 0 || n === 1 || n === 3) return [[]];
     throw new Error("Should be here");
     ;
 }
--- src/progtest.ts
+++
@@ -1,4 +0,0 @@
-import { solveNQueen } from "./nQueenNoAttack/nQueen";
-
-const testValues = [0, 1, 2, 4];
-testValues.forEach(solveNQueen);
