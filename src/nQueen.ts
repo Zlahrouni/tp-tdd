ddd12d8cfb83ce24484150a5df6210b4ad0bf8ec
= <=>
2025-02-12 14:58:00 +0100

    fix base solution

--- src/nQueen.ts
+++ src/nQueen.ts
@@ -4,8 +4,8 @@ export function solveNQueen(n: number): string[] {
     if (n < 0) throw new Error("N should'nt be negative");
     if (n === 0) return [];
     if (n === 1) return ['#'];
-    if (n === 2) return [];
-    
+    if (n < 4) return [];
+
     // create a board
     const board: Board = Array(n).fill(null).map(() => Array(n).fill('O'));
     console.log(`board ${n} : \n`, board);
