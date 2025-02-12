6bcdc8fc0b160aeac15f31a494f668f9d5933f7b
= <=>
2025-02-12 16:51:53 +0100

    test for value that will give empty 1 & 3

--- src/nQueenAttack/nQueenAttack.test.ts
+++ src/nQueenAttack/nQueenAttack.test.ts
@@ -5,4 +5,13 @@ describe("nQueen Attack test", () => {
     test("it should throw error : n should'nt be negative", () => {
         expect(() => solveNQueenAtack(-2)).toThrow("n should'nt be negative")
     });
+
+    test("should return empty array for n = 0", () => {
+        expect(solveNQueenAtack(0)).toEqual([[]])
+    })
+
+    test("should return empty array for n = 1 & 3", () => {
+        expect(solveNQueenAtack(1)).toEqual([[]]);
+        expect(solveNQueenAtack(3)).toEqual([[]]);
+    });
 })
--- src/nQueenAttack/nQueenAttack.ts
+++ src/nQueenAttack/nQueenAttack.ts
@@ -1,3 +1,6 @@
 export function solveNQueenAtack(n: number): string[][] {
-    return [[]];
+    if (n < 0) throw new Error("n should'nt be negative");
+    if (n === 1) return [[]];
+    throw new Error("Should be here");
+    ;
 }
